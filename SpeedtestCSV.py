77e6c76ad1023bb5d9873cf401a7dff64b87bdf6
Nimbus1939 <84851834+Nimbus1939@users.noreply.github.com>
2021-05-27 12:56:06 +0200

    Update SpeedtestCSV.py

--- SpeedtestCSV.py
+++ SpeedtestCSV.py
@@ -13,5 +13,5 @@ with open(file_path) as csv_file:
 		print(f"P \"Internet ping\" ping={row[0]};20;500;0;1000 Pingtime {row[0]}ms")
 		print(f"P \"Internet download\" download={row[1]};600:9999;300:9999 Download speed {row[1]} Mb/s")
 		print(f"P \"Internet upload\" upload={row[2]};80:9999;50:9999 Upload speed {row[2]} Mb/s")
-		# The line below will produce one service with three graphs if you do not want them all in one comment out the next line, instead of three services.
+		# The line below will produce one service with three graphs instead of three services, if you do not want them all in one service comment out the next line.
 		print(f"P \"Internet speed\" Download={row[1]};600:1000;300:1000|Upload={row[2]};80:1000;50:1000|PING={row[0]};20;500")
