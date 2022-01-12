e2040e388b5888887a763abc73ceb7b9c1e16214
Nimbus1939 <84851834+Nimbus1939@users.noreply.github.com>
2022-01-12 10:11:07 +0100

    Create InternetMetrics.py

---
+++ InternetMetrics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+metric_info["HDownload"] = {
+    "title": _("Download"),
+    "unit": "bits/s",
+    "color": "#231bb5",
+}
+metric_info["HUpload"] = {
+    "title": _("Upload"),
+    "unit": "bits/s",
+    "color": "#006040",
+}
